afcc9d69990ddbec6f0bcf656e259b14220785a6
EmaElsayed <emoelsayed78@gmail.com>
2023-07-27 22:53:03 +0300

    h

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:project3/ui/onboarding_screen.dart';
+
+//jhhhh
+
+void main() async {
+
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+
+    );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Onboarding Screen',
+      home: OnboardingScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
